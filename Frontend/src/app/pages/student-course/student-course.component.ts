87d9610aaa102dce6c4a5ed73e7f780ee421b1ab
KarimChehab2003 <karimchehab2018@gmail.com>
2025-05-13 15:56:03 +0300

    - Added placeholder for student-course page

--- Frontend/src/app/pages/student-course/student-course.component.ts
+++ Frontend/src/app/pages/student-course/student-course.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { TopbarComponent } from "./topbar/topbar.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { ContentComponent } from "./content/content.component";
 
 @Component({
   selector: 'app-student-course',
-  imports: [],
+  imports: [TopbarComponent, SidebarComponent, ContentComponent],
   templateUrl: './student-course.component.html',
   styleUrl: './student-course.component.css'
 })
